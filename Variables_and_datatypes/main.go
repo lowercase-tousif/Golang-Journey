f2842c0b805b28394730bf7a587a5e8894b59fc9
lowercase-tousif <toisiftasrik@gmail.com>
2025-01-14 21:44:38 +0600

    Variables and Datatypes done

---
+++ Variables_and_datatypes/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10 // variable defining
+	fmt.Println(a)
+
+	/*
+		int
+		float32
+		string
+		bool
+	*/
+
+	// var x int = 105
+	// fmt.Println(x)
+
+	y := "tousif tasrik"
+	fmt.Println(y)
+
+	// y = 56.7 // can't do that because in 19 line we have defined := using this that the datatype of the y should be string
+
+	y = "Hello,World!"
+	fmt.Println(y)
+
+	var x bool = true
+	fmt.Println(x)
+}
