a429f9a79eb99d9b54c77561675acfb4a19f55a5
lowercase-tousif <toisiftasrik@gmail.com>
2025-01-18 00:01:28 +0600

    Win sequences and lose sequences done

--- PROJECTS/CLI/ROCK_PAPER_SCISSOR_PROJECT/main.go
+++ PROJECTS/CLI/ROCK_PAPER_SCISSOR_PROJECT/main.go
@@ -6,4 +6,26 @@ import (
 
 func main(){
 	fmt.Println("Welcome to Rock Paper Scissor Game");
-}
+
+}	
+
+
+/*
+   BREAKDOWN OF THE WINNING AND LOSING SCENARIOS
+
+   WIN SEQUENCES
+
+   Rock --> Lizard, Scissors
+   Paper --> Rock, Spock
+   Scissors --> Paper, Lizard
+   Lizard --> Spock, Paper
+   Spock --> Scissors, Rock
+
+   LOSE SEQUENCES
+
+   Rock --> Paper, Spock
+   Paper --> Scissors, Lizard
+   Scissors --> Rock, Spock
+   Lizard --> Rock, Scissors
+   Spock --> Lizard, Paper
+*/
